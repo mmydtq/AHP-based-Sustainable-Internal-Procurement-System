137fc96e4ec7071bb761fe1a168fccc152c7568e
eric1112222 <1500558480@qq.com>
2024-03-21 17:59:15 +0800

    from xing

--- backEnd/init.py
+++ backEnd/init.py
@@ -2,18 +2,18 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# MySQL configurations
-# app.config['MYSQL_HOST'] = '10.19.32.179'
-# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_HOST'] = 'LAPTOP-12893DCP'
+# app.config['MYSQL_USER'] = 'eric'
 # app.config['MYSQL_PASSWORD'] = 'Xingyangniubi1!'
 # app.config['MYSQL_DB'] = 'group9'
 # mysql = MySQL(app)
+# conn = MySQLdb.connect(host='LAPTOP-12893DCP', user='eric', password='Xingyangniubi1!', database='group9')
 # setRouter(app,mysql)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:123@10.19.32.179:3306/group9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://eric:Xingyangniubi1!@LAPTOP-12893DCP:3306/group9?connect_timeout=10'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# setRouter(app)
+#setRouter(app)
 
 class test(db.Model):
     testid = db.Column(db.Integer, primary_key=True)

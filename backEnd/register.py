288a52a25a385d8dc354ba8c97f0ff5ca73495f4
eric1112222 <1500558480@qq.com>
2024-03-27 16:41:36 +0800

    register and login

---
+++ backEnd/register.py
@@ -0,0 +1,41 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse
+from user import User
+
+class Register(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('password', type=str, required=True)
+        parser.add_argument('phone', type=int, required=True)
+        parser.add_argument('room', type=str, required=True)
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('uName', type=str, required=True)
+        args = parser.parse_args()
+
+        status = User.findRepeat(args['uName'], args['room'], args['email'], args['phone'])
+        if status == 0:
+            User.addUser(args['uName'], args['room'], args['email'], args['phone'], args['password'])
+
+        return jsonify({"status": status}), 200
+    
+class Login(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('uName', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+
+        status = User.checkLogin(args['uName'], args['password'])
+
+        return jsonify({"status": status}), 200
+    
+class RePassword(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('uName', type=str, required=True)
+        parser.add_argument('rePassword', type=str, required=True)
+        args = parser.parse_args()
+
+        status = User.rePassword(args['uName'], args['rePassword'])
+
+        return jsonify({"status": status}), 200
--- backEnd/router.py
+++ backEnd/router.py
@@ -1,17 +1,22 @@
-from flask import jsonify
+# 用于测试的类，没有实质作用
 from database import db;
+from flask_restful import Resource
 
 class test(db.Model):
     testid = db.Column(db.Integer, primary_key=True)
 
-def setRouter(app):
-    @app.route('/')
-    def database_test():
-        try:
-            result = test.query.first()
-            return "Database connection successful!"
-        except Exception as e:
-            return "Database connection failed: " + str(e)
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+# def setRouter(app):
+#     @app.route('/')
+#     def database_test():
+#         try:
+#             result = test.query.first()
+#             return "Database connection successful!"
+#         except Exception as e:
+#             return "Database connection failed: " + str(e)
 
 
 # def setRouter(app):

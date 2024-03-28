083c779d3f494527d6805d104cba96d4715ed571
Moonquakeyu <101801812+Moonquakeyu@users.noreply.github.com>
2024-03-28 23:14:27 +0800

    增加点击bottom组件图片进入“value”页

--- my-app-test/src/component/Bottom/index.tsx
+++ my-app-test/src/component/Bottom/index.tsx
@@ -3,6 +3,7 @@ import { Divider} from 'antd';
 import logo from "@/assert/Bottom.png";
 import styled from "./index.module.css"
 import Image from "next/image" ;
+import Link from "next/link" ;
 
 
 const Bottom: React.FC = () => (
@@ -11,8 +12,14 @@ const Bottom: React.FC = () => (
     <Divider />
     <p className={styled.sentence} >Nature, to be commanded, must be obeyed.</p>
     <p className={styled.sentence2} >Make integer Fly~</p>
-    <Image src={logo} alt="logo" width={100} height={100} className={styled.logo} />
+    <Link  href="/value">
+      <div>
+        <Image src={logo} alt="logo" width={100} height={100} className={styled.logo} />
+        </div> 
+        
     
+    </Link>
+   
 
   </div>
 );

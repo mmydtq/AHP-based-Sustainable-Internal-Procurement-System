8bbf6a5b2a960ca37ca59f83cdc64815344cb57b
Liangzi-Wang <quantum0119@163.com>
2024-04-10 01:27:49 +0800

    addgoods

--- backEnd/admin.py
+++ backEnd/admin.py
@@ -1,3 +1,6 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse
+
 from backEnd.good import Good
 from database import db
 
@@ -13,23 +16,23 @@ class Admin(db.Model):
 
     @staticmethod
     def findRepeat(name, phone, room, email):
-        existing_user = Admin.query.filter_by(name = name).first()
+        existing_user = Admin.query.filter_by(name=name).first()
         if existing_user:
             return 1  # 用户名已经存在
-        existing_phone = Admin.query.filter_by(phone = phone).first()
+        existing_phone = Admin.query.filter_by(phone=phone).first()
         if existing_phone:
             return 2  # 电话已经存在
-        existing_room = Admin.query.filter_by(room = room).first()
+        existing_room = Admin.query.filter_by(room=room).first()
         if existing_room:
             return 3  # 科室已存在
-        existing_email = Admin.query.filter_by(email = email).first()
+        existing_email = Admin.query.filter_by(email=email).first()
         if existing_email:
             return 4  # 邮箱已存在
         return 0  # 账号注册成功
 
     @staticmethod
     def addAdmin(name, room, email, phone, password):
-        new_admin = Admin(name=name, room = room, email=email, phone = phone, password=password)
+        new_admin = Admin(name=name, room=room, email=email, phone=phone, password=password)
         db.session.add(new_admin)
         db.session.commit()
 
@@ -44,11 +47,32 @@ class Admin(db.Model):
 
     @staticmethod
     def add_good(good_id, url, environmentalValue, brief, tag, name, value, description, hint):
-        g = Good.query.filter_by(url = url).first()
+        g = Good.query.filter_by(url=url).first()
         if not g:
             new_good = Good(id=good_id, url=url, environmental_value=environmentalValue, brief=brief, tag=tag,
                             name=name, value=value, description=description, hint=hint)
             db.session.add(new_good)
             db.session.commit()
-            return 0 # 修改成功
+            return 0  # 修改成功
         return 1
+
+
+class AddGoods(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('good_id', type=int, required=True)
+        parser.add_argument('url', type=str, required=True)
+        parser.add_argument('environmentalValue', type=int, required=True)
+        parser.add_argument('brief', type=str, required=True)
+        parser.add_argument('tag', type=str, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('value', type=float, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('hint', type=int, required=True)
+        data = parser.parse_args()
+
+        status = Admin.add_good(data['good_id'], data['url'], data['environmentalValue'], data['brief'], data['tag'],
+                                data['name'],
+                                data['value'], data['description'], data['hint'])
+
+        return jsonify({"status": status}), 200

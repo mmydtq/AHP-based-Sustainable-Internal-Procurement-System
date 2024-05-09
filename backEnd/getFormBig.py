656d3356c6cf71e9c4e0ce8534c553182b9b97bb
YuLinglong21207457 <linglong.yu@ucdconnect.ie>
2024-05-09 19:51:54 +0800

    Update getFormBig.py

--- backEnd/getFormBig.py
+++ backEnd/getFormBig.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import json
+from item import Item
 from flask_cors import cross_origin
 from database import db
 
@@ -11,43 +12,22 @@ class GetFormBig(Resource):
     @cross_origin()
     def post(self):
         try:
-            # Create a request parser to validate incoming data
-            parser = reqparse.RequestParser()
-            parser.add_argument('key', type=str, help='Key for identification')
-            parser.add_argument('name', type=str, help='Name of the item')
-            parser.add_argument('value', type=int, help='Value of the item')
-            parser.add_argument('address', type=str, help='Address or department')
-            parser.add_argument('tags', type=list, location='json', help='Tags associated with the item')
-            parser.add_argument('brief', type=str, help='Brief description of the item')
-            parser.add_argument('number', type=int, help='Quantity or count of the item')
-
-            # Parse the incoming request data
-            args = parser.parse_args()
-
-            # Extract the arguments
-            key = args.get('key', 'default_key')  # Provide a default value if none is given
-            name = args.get('name', 'Default Name')
-            value = args.get('value', 0)
-            address = args.get('address', 'Default Address')
-            tags = args.get('tags', [])
-            brief = args.get('brief', 'Default Brief')
-            number = args.get('number', 0)
-
-            # Convert tags list to JSON string
-            tags_json = json.dumps(tags)
-
-            # Construct the response data based on the provided specification
+            # Query all items from the database
+            items = db.session.query(Item).all()
+            
+            # Transform the items into the desired response format
             response_data = {
                 "data": [
                     {
-                        "key": key,
-                        "name": name,
-                        "value": value,
-                        "address": address,
-                        "tag": tags_json,
-                        "brief": brief,
-                        "number": number
+                        "key": item.key,
+                        "name": item.name,
+                        "value": item.value,
+                        "address": item.address,
+                        "tag": json.loads(item.tag),  
+                        "brief": item.category,  
+                        "number": item.number
                     }
+                    for item in items
                 ]
             }
 
@@ -55,6 +35,7 @@ class GetFormBig(Resource):
             return response_data, 200
 
         except Exception as e:
+            # Return an error response with an appropriate message
             return {"error": str(e)}, 400
         
 class DeleteFormItem(Resource):

5d9c58f7a054a91acfd12067c6b6381cef423c58
Liangzi-Wang <quantum0119@163.com>
2024-05-15 18:18:00 +0800

    fixed predict

--- backEnd/getLineChart.py
+++ backEnd/getLineChart.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_cors import cross_origin
 from sqlalchemy import desc
+from database import db
 
 from order import Order
 
@@ -8,8 +9,9 @@ class get_line_chart(Resource):
     @cross_origin()
     def post(self):
         try:
-            sorted_orders = Order.query.order_by(desc(Order.date)).all()
+            sorted_orders = db.session.query(Order).order_by(Order.date.desc())
             # 创建一个空字典，用于存储日期和订单列表的映射
+            sorted_orders = list(sorted_orders)
             date_order_dict = {}
 
             # 遍历已排序的订单
@@ -21,21 +23,21 @@ class get_line_chart(Resource):
 
             # 存储过去的月份
             months = []
-            for date in list(date_order_dict.keys())[:12]:
+            for date in list(date_order_dict.keys())[:7]:
                 months.append(date)
 
             # 存储每个月的环境值和销售值
             monthly_env = []
             monthly_sale = []
             monthly_order_number = []
-            for orders in list(date_order_dict.values())[:12]:
+            for orders in list(date_order_dict.values())[:7]:
                 env_value = 0
                 sale_value = 0
                 order_num = 0
                 for order in orders:
                     if order.state == 1:
                         order_num = order_num + 1
-                        goods = order.goods.all()
+                        goods = list(order.goods)
                         for good in goods:
                             env_value = env_value + good.environmental_value
                             sale_value = sale_value + good.value
--- backEnd/getPredictNum.py
+++ backEnd/getPredictNum.py
@@ -6,16 +6,17 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import json
 from flask_cors import cross_origin
+from database import db
 from order import Order
 
 class getPredictNum(Resource):
     @cross_origin()
     def post(self):
         try:
-            sorted_orders = Order.query.order_by(desc(Order.date)).all()
+            sorted_orders = db.session.query(Order).order_by(Order.date.desc())
             # 创建一个空字典，用于存储日期和订单列表的映射
+            sorted_orders = list(sorted_orders)
             date_order_dict = {}
-
             # 遍历已排序的订单
             for order in sorted_orders:
                 order_date = order.date  # 获取订单的日期
@@ -26,12 +27,12 @@ class getPredictNum(Resource):
             # 存储每个月的环境值和销售值
             monthly_env=[]
             monthly_sale=[]
-            for orders in list(date_order_dict.values())[:12]:
+            for orders in list(date_order_dict.values())[:7]:
                 env_value = 0
                 sale_value = 0
                 for order in orders:
                     if order.state == 1:
-                        goods = order.goods.all()
+                        goods = list(order.goods)
                         for good in goods:
                             env_value = env_value + good.environmental_value
                             sale_value = sale_value + good.value
@@ -62,12 +63,18 @@ class getPredictNum(Resource):
             #环境值变化百分比
             index_of_lastenv = len(monthly_env.values) - 1
             env_change = next_month_env-monthly_env.values[index_of_lastenv]
-            env_change_per = (env_change/monthly_env.values[index_of_lastenv]) * 100
+            if monthly_env.values[index_of_lastenv] == 0:
+                env_change_per = (env_change/1)*100
+            else:
+                env_change_per = (env_change/monthly_env.values[index_of_lastenv]) * 100
 
             #交易数变化百分比
             index_of_lastsale = len(monthly_sale.values) - 1
             sale_change = next_month_sales - monthly_sale.values[index_of_lastsale]
-            sale_change_per = (sale_change / monthly_sale.values[index_of_lastsale]) * 100
+            if monthly_sale.values[index_of_lastsale]==0:
+                sale_change_per = (sale_change/1)*100
+            else:
+                sale_change_per = (sale_change / monthly_sale.values[index_of_lastsale]) * 100
 
             response_data = {"value": [next_month_env, next_month_sales, env_change_per, sale_change_per]}
             return response_data, 200

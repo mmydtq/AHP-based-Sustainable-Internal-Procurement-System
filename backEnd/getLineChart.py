3a53cf8a8525fc5d010c5b0858768d10d25d5e7c
Liangzi-Wang <quantum0119@163.com>
2024-05-15 14:19:05 +0800

    change linechart

--- backEnd/getLineChart.py
+++ backEnd/getLineChart.py
@@ -1,17 +1,53 @@
 from flask_restful import Resource, reqparse
 from flask_cors import cross_origin
+from sqlalchemy import desc
+
+from order import Order
 
 class get_line_chart(Resource):
     @cross_origin()
     def post(self):
         try:
+            sorted_orders = Order.query.order_by(desc(Order.date)).all()
+            # 创建一个空字典，用于存储日期和订单列表的映射
+            date_order_dict = {}
 
+            # 遍历已排序的订单
+            for order in sorted_orders:
+                order_date = order.date  # 获取订单的日期
+                if order_date not in date_order_dict:
+                    date_order_dict[order_date] = []  # 如果日期不在字典中，创建一个空列表
+                date_order_dict[order_date].append(order)  # 将订单添加到对应日期的列表中
+
+            # 存储过去的月份
+            months = []
+            for date in list(date_order_dict.keys())[:12]:
+                months.append(date)
+
+            # 存储每个月的环境值和销售值
+            monthly_env = []
+            monthly_sale = []
+            monthly_order_number = []
+            for orders in list(date_order_dict.values())[:12]:
+                env_value = 0
+                sale_value = 0
+                order_num = 0
+                for order in orders:
+                    if order.state == 1:
+                        order_num = order_num + 1
+                        goods = order.goods.all()
+                        for good in goods:
+                            env_value = env_value + good.environmental_value
+                            sale_value = sale_value + good.value
+                monthly_order_number.append(order_num)
+                monthly_env.append(env_value)
+                monthly_sale.append(sale_value)
 
             response_data = {
-                "date": args['xAxisData'],
-                "value1": args['seriesData1'],
-                "value2": args['seriesData2'],
-                "value3": args['seriesData3']
+                "date": months,
+                "value1": monthly_sale,
+                "value2": monthly_env,
+                "value3": monthly_order_number
             }
 
             return response_data, 200
